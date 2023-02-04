14483bf1fd0c330d722a99a17544b555659d1152
AlexZurovna <AlexandraZaurovna@gmail.com>
2023-02-04 21:20:00 +0300

    add module 1 lesson 6 step 5 task

---
+++ lesson6_step5.py
@@ -0,0 +1,39 @@
+# module 1 lesson 6 step 5
+
+# full task:
+# На указанной ниже странице вам нужно найти зашифрованную ссылку и кликнуть по ней.
+# Далее заполнить форму на этой странице с помощью Selenium.
+# Если всё сделано правильно, то вы увидите окно с проверочным кодом. 
+# Это число вам нужно ввести в качестве ответа в этой задаче.
+# form link: http://suninjuly.github.io/find_link_text
+
+# Remember!
+# code will be useless after 30 seconds
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    link_name = str(math.ceil(math.pow(math.pi, math.e)*10000))
+
+    input1 = browser.find_element(By.TAG_NAME, 'input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, 'last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.PARTIAL_LINK_TEXT, link_name)
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
