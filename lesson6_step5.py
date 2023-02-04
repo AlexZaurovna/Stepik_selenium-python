e2fa91545547aed75dddfdecb600630229a46090
AlexZurovna <AlexandraZaurovna@gmail.com>
2023-02-04 21:46:36 +0300

    modifier code

--- lesson6_step5.py
+++ lesson6_step5.py
@@ -13,13 +13,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time 
+import math
 
 link = "http://suninjuly.github.io/find_link_text"
+link_text = str(math.ceil(math.pow(math.pi, math.e)*10000))
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-    link_name = str(math.ceil(math.pow(math.pi, math.e)*10000))
+
+    link = browser.find_element(By.LINK_TEXT, link_text)
+    link.click()
 
     input1 = browser.find_element(By.TAG_NAME, 'input')
     input1.send_keys("Ivan")
@@ -29,7 +33,7 @@ try:
     input3.send_keys("Smolensk")
     input4 = browser.find_element(By.ID, "country")
     input4.send_keys("Russia")
-    button = browser.find_element(By.PARTIAL_LINK_TEXT, link_name)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
