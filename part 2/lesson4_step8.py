11c8e021a9e200f9fb62864971e923604953faf4
AlexZaurovna <31659078+AlexZaurovna@users.noreply.github.com>
2023-03-24 18:35:30 +0300

    add lib import and edit script
    add library "WebDriverWait" import and edit script by "price" and "button1"

--- part 2/lesson4_step8.py
+++ part 2/lesson4_step8.py
@@ -17,6 +17,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
@@ -29,10 +30,12 @@ try:
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
-    price = webdriverwait(browser, 5).until(
-        EC.text_to_be_present_in_element((By.ID, "price"))
+    price = WebDriverWait(browser, 10).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
     )
     
+    button1 = browser.find_element(By.ID, "book")
+    button1.click()
 
     x_element = browser.find_element(By.ID, 'input_value')
     x = x_element.text
